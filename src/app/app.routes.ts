5801ac0b96ef8ae47f3200a309a2f9076e31f66f
Felipe Torres Cresto <felipe.t.cresto@gmail.com>
2024-07-08 04:50:09 -0300

    feat: create hom component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { HomeComponent } from './components/home/home.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('./components/home/home.component').then(c => c.HomeComponent),
+    },
+];
